511f369859c502385430d4c196a57e82afed06d8
Maciej Zubala <maciej.zubala@allegro.pl>
2020-03-10 22:52:13 +0100

    prep

--- src/test/groovy/pl/com/bottega/sodamachine/StandardMachineControllerSpec.groovy
+++ src/test/groovy/pl/com/bottega/sodamachine/StandardMachineControllerSpec.groovy
@@ -3,14 +3,17 @@ package pl.com.bottega.sodamachine
 import io.vavr.control.Try
 import spock.lang.Specification
 
+import static pl.com.bottega.sodamachine.Driver.*
+
 class StandardMachineControllerSpec extends Specification {
 
     private static final COLA = new Drink((byte) 1, "Coca-Cola", new Money(250));
-    private static final FANTA = new Drink((byte) 1, "Fanta", new Money(250));
-    private static final SPRITE = new Drink((byte) 1, "Sprite", new Money(250));
+    private static final FANTA = new Drink((byte) 2, "Fanta", new Money(250));
+    private static final SPRITE = new Drink((byte) 3, "Sprite", new Money(250));
     public static final String WELCOME_TEXT = "Good Morning! Always Coca-Cola!"
     public static final String THANK_YOU_TEXT = "Thank you! Please collect your drink."
     public static final String THANK_YOU_TEXT_WITH_CHANGE = "Thank you! Please collect your drink and change."
+    public static final Try<Object> SUCCESS = Try.success(null)
     private Dispenser dispenser = Mock(Dispenser)
     private Ledger ledger = Mock(Ledger)
     private Display display = Mock(Display)
@@ -18,7 +21,8 @@ class StandardMachineControllerSpec extends Specification {
 
 
     void setup() {
-        drinkRepository.givenDrinks(COLA)
+        drinkRepository.givenDrinks(COLA, FANTA, SPRITE)
+        display._ >> SUCCESS
     }
 
     def "displays welcome text after creation"() {
@@ -39,28 +43,28 @@ class StandardMachineControllerSpec extends Specification {
             controller.drinkButtonPressed(COLA.nr)
 
         then:
-            1 * display.displayBriefly("${COLA.name}: ${COLA.price}")
+            1 * display.displayBriefly("${COLA.name}: ${COLA.price}") >> SUCCESS
             0 * ledger._
             0 * dispenser._
     }
 
     def "takes money and dispenses the drink and displays messages"() {
         given:
-            ledger.acceptCoins() >> Try.success(null)
-            dispenser.dispense(_) >> Try.success(null)
+            ledger.acceptCoins() >> SUCCESS
+            dispenser.dispense(_) >> SUCCESS
             StandardMachineController controller = aController()
 
         when:
             controller.coinInserted(new Money(200))
 
         then:
-            1 * display.displayPermanently("Amount: ${new Money(200)}")
+            1 * display.displayPermanently("Amount: ${new Money(200)}") >> SUCCESS
 
         when:
             controller.coinInserted(new Money(50))
 
         then:
-            1 * display.displayPermanently("Amount: ${new Money(250)}")
+            1 * display.displayPermanently("Amount: ${new Money(250)}") >> SUCCESS
 
         when:
             controller.drinkButtonPressed(COLA.nr)
@@ -82,22 +86,25 @@ class StandardMachineControllerSpec extends Specification {
             controller.drinkButtonPressed(COLA.nr)
 
         then:
-            1 * display.displayPermanently("Amount: ${new Money(200)}")
-            1 * display.displayBriefly("${COLA.name}: ${COLA.price}")
+            1 * display.displayPermanently("Amount: ${new Money(200)}") >> SUCCESS
+            1 * display.displayBriefly("${COLA.name}: ${COLA.price}") >> SUCCESS
     }
 
     def "dispenses the coins back when user presses the cancel button"() {
-        given:
+        when:
             StandardMachineController controller = aController()
+
+        then:
+            1 * display.displayPermanently(WELCOME_TEXT)
 
         when:
             controller.coinInserted(new Money(200))
             controller.cancelButtonPressed()
 
         then:
-            1 * ledger.rejectCoins()
+            1 * ledger.rejectCoins() >> SUCCESS
             0 * ledger._
-            2 * display.displayPermanently(WELCOME_TEXT)
+            1 * display.displayPermanently(WELCOME_TEXT)
             0 * dispenser._
     }
 
@@ -112,8 +119,8 @@ class StandardMachineControllerSpec extends Specification {
         then:
             1 * ledger.acceptCoins()
             1 * ledger.dispense(new Money(250))
-            1 * display.displayPermanently("Amount: ${new Money(500)}")
-            1 * display.displayBrieflyAndThePermanently(THANK_YOU_TEXT_WITH_CHANGE, WELCOME_TEXT)
+            1 * display.displayPermanently("Amount: ${new Money(500)}") >> SUCCESS
+            1 * display.displayBrieflyAndThePermanently(THANK_YOU_TEXT_WITH_CHANGE, WELCOME_TEXT) >> SUCCESS
             1 * dispenser.dispense(COLA)
     }
 
@@ -127,6 +134,21 @@ class StandardMachineControllerSpec extends Specification {
         then:
             0 * ledger._
             0 * dispenser._
+    }
+
+    def "it is possible to continue purchase if rejecting coins fails"() {
+        given:
+            StandardMachineController controller = aController()
+            ledger.rejectCoins() >> Try.failure(new DriverException(1))
+
+        when:
+            controller.coinInserted(new Money(500))
+            controller.cancelButtonPressed()
+            controller.drinkButtonPressed(FANTA.nr)
+
+        then:
+            1 * dispenser.dispense(FANTA)
+            1 * ledger.acceptCoins()
     }
 
     private StandardMachineController aController() {
